bb69bf7daf07608a08d3c0f652675606a9820095
1945680782-wind <1945680782@qq.com>
2019-09-23 21:05:57 +0800

    第二次上传
    第二次上传
    
    Signed-off-by: 1945680782-wind <1945680782@qq.com>

--- 1945680782-wind/ConsoleApplication1/UnitTest1/unittest1.cpp
+++ 1945680782-wind/ConsoleApplication1/UnitTest1/unittest1.cpp
@@ -1,18 +1,40 @@
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
-
+#include <assert.h>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
+double run1(char c, double d1, double d2)//计算四则运算
+{
+	switch (c)
+	{
+	case '+':
+		return d1 + d2;
+		break;
+	case '-':
+		return d1 - d2;
+		break;
+	case'*':
+		return d1 * d2;
+		break;
+	case '/':
+		return d1 / d2;
+		break;
+	default:
+		return 0.0;
+		break;
+	}
+}
 namespace UnitTest1
 {		
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
 			// TODO: 在此输入测试代码
+			double i = run1('+', 2, 3);
+			assert(i,5);
 		}
-
 	};
 }
